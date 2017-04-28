c3d7bdbe219ac40d700c33d4ee917649272df200
Casey M <caseym@gmail.com>
2017-04-27 20:23:13 -0400

    refactor

--- app.py
+++ app.py
@@ -24,13 +24,13 @@ def get_contact(fname, lname):
     with open('sample_contacts.json') as data_file:
         contacts = json.load(data_file)
 
-    # loop through contacts and look for name that user said
+    # loop through contacts and find name
     for contact in contacts:
-        if contact['name'] == '{} {}'.format(fname, lname):
-            description = contact['description']
+        if contact['name'] == '{} {}'.format(fname.title(), lname.title()):
+            speech_text = contact['description']
+        else:
+            speech_text = "Sorry I cannot find that contact."
 
-    # return description as speech text
-    speech_text = description
     return statement(speech_text)
 
 
